1dd22bf6cc2083baae968b3a4ebaa3c675a8e93a
chienbv <chienlgend24032004@gmail.com>
2026-05-27 16:10:10 +0700

    Submit task 1 - Score: 10.0

--- task1.cpp
+++ task1.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 int main() {
-    // Your code here
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+
+    for (int i = 0; i < n; i++) {
+        cout << a[i];
+        if (i < n - 1) cout << " ";
+    }
+
     return 0;
 }
